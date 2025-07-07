884866247eaa28991f8bf4faeaae7e3483e97a99
h.nasiri <h.nasiri@rayanhamafza.com>
2025-07-07 09:45:46 +0330

    first commit

---
+++ src/main/java/org/zarhub/ZarhubApplication.java
@@ -0,0 +1,13 @@
+package org.zarhub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZarhubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZarhubApplication.class, args);
+	}
+
+}
